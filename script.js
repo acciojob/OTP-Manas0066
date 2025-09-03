ff2e712d0d0e7282f8babf01dbc8c43091544408
Manas Chaudhary <manaschaudhary1602@gmail.com>
2025-09-03 14:24:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+const otp = document.querySelectorAll(".code");
+// console.log(otp);
+
+// console.log(otp[0]);
+// console.dir(otp[0]);
+
+otp[0].focus();
+
+otp.forEach((num, index) => {
+    // console.log(num);
+    num.addEventListener("input", (event) => {
+        // console.log(event);
+        console.log(event.target.value);
+        if(event.target.value.length === 1 && index < otp.length - 1) {
+            otp[index + 1].focus();
+        }
+    })
+
+    num.addEventListener("keydown", (event) => {
+        // console.log(event);
+        if(event.key === "Backspace" && index > 0) {
+            otp[index - 1].focus();
+        }
+    })
+})
